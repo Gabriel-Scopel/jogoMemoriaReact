3fc0463700a8d56870e07c93dd6e288bd0cd3ddb
Gabriel de Souza Scopel <93132666+Gabriel-Scopel@users.noreply.github.com>
2023-03-16 17:59:51 -0300

    criando botão, itens e exibindo grid

--- react_memoria/src/App.tsx
+++ react_memoria/src/App.tsx
@@ -1,8 +1,58 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles';
 import logoImage from './assets/devmemory_logo.png';
+import { Button } from './components/Button';
 import {InfoItem} from './components/InfoItem';
+import RestartIcon from './svgs/restart.svg';
+import { GridItemType } from './types/GridItemType';
+import {items} from './data/items';
+import { GridItem } from './components/GridItem';
 
 const App = ()=>{
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [showCount, setShowCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => resetAndCreateGrid(), []);
+
+  const resetAndCreateGrid = ()=>{
+    //passo 1: resetar o jogo
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShowCount(0);
+
+    //passo 2: criar o grid vazio
+    let tmpGrid: GridItemType[] = [];
+    for(let i=0; i<(items.length*2); i++){
+      tmpGrid.push({
+        item:null,
+        shown:false,
+        permanentShown: false
+      });
+    }
+    //2.1: preencher o grid:
+    for(let w = 0; w<2 ; w++){
+      for(let i = 0; i< items.length; i++){
+        let pos = -1;
+        while(pos < 0 || tmpGrid[pos].item !== null){
+          pos = Math.floor(Math.random()*(items.length)*2);
+        }
+        tmpGrid[pos].item = i;
+      }
+    }
+    //2.2: jogar no state:
+    setGridItems(tmpGrid);
+
+    //passo 3: começar o jogo
+    setPlaying(true);
+  }
+
+  const handleItemclick = (index:number)=>{
+
+  }
+ 
   return(
     <C.Container>
       <C.Info>
@@ -14,11 +64,20 @@ const App = ()=>{
           <InfoItem label='Tempo' value='00:00'></InfoItem>
           <InfoItem label='Movimentos' value='0'></InfoItem>
         </C.InfoArea>
-        <button>reinicar</button>
+        <Button label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid}></Button>
       </C.Info>
 
       <C.GridArea>
-        ...
+        <C.Grid> 
+          {gridItems.map((item, index)=>(
+            <GridItem
+            key={index}
+            item={item}
+            onClick={()=> handleItemclick(index)}
+            />
+          ))}
+
+        </C.Grid>
       </C.GridArea>
 
     </C.Container>
